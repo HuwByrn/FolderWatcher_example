6cefda7f26a008b565463368df8eac8c4e24d0c5
Huw Byrne-Jones <huw.byrne-jones@itv.com>
2013-05-07 22:17:53 +0100

    Initial commit

---
+++ src/com/plus6/services/FolderWatchService.java
@@ -0,0 +1,92 @@
+package com.plus6.services;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Huw
+ * Date: 22/04/13
+ * Time: 17:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class FolderWatchService {
+    private WatchService watcher;
+    private Map<WatchKey, Path> keys;
+    public Boolean stop = false;
+
+    public FolderWatchService() {
+        keys = new HashMap<WatchKey, Path>();
+    }
+
+    public void destroy() throws IOException {
+
+        watcher.close();
+        watcher = null;
+
+        keys.clear();
+        keys = null;
+    }
+
+    public void register(Path path) {
+        try {
+
+            watcher = FileSystems.getDefault().newWatchService();
+        } catch (IOException e) {
+            // handle
+            System.err.println(e);
+
+        }
+        WatchKey key = null;
+        try {
+            key = path.register(watcher, ENTRY_CREATE, ENTRY_MODIFY);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        keys.put(key, path);
+    }
+
+    public WatchService getWatcher() {
+        return watcher;
+    }
+
+    public Map<WatchKey, Path> getKeys() {
+        return keys;
+    }
+    public void kill(){
+       this.stop = true;
+    }
+    public void run() {
+        while (false == this.stop) {
+
+            WatchKey key;
+
+            System.out.println("WatchKey request before try");
+            try {
+                System.out.println("WatchKey request in try ");
+                key = this.watcher.take();
+
+            } catch (InterruptedException x) {
+                System.err.println("WatchKey request fail");
+                break;
+
+//                return;
+            }
+            Path dir = keys.get(key);
+            System.out.println("WatchKey request path to string " + dir.toString());
+            if (dir == null) {
+                         System.err.println("WatchKey not recognized!!");
+                         continue;
+                     }
+        }
+    }
+}
---
+++ test/com/plus6/services/FolderWatchServiceTest.java
@@ -0,0 +1,115 @@
+package com.plus6.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Huw
+ * Date: 22/04/13
+ * Time: 17:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class FolderWatchServiceTest {
+    private FolderWatchService sut;
+    @Mock
+    private Path pathDependency;
+
+    @Before
+    public void setUp() throws Exception {
+        sut = new FolderWatchService();
+        pathDependency = mock(Path.class);
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        sut.destroy();
+        reset(pathDependency);
+    }
+
+    @Test
+    public void register_should_make_a_WatchService() {
+        sut.register(pathDependency);
+        assertThat(sut.getWatcher(), is(WatchService.class));
+    }
+
+    @Test
+    public void register_should_register_watcher_with_given_Watchable() {
+        sut.register(pathDependency);
+        ArgumentCaptor<WatchEvent.Kind> argument = ArgumentCaptor.forClass(WatchEvent.Kind.class);
+        try {
+            verify(pathDependency).register(eq(sut.getWatcher()), argument.capture(), argument.capture());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    @Test
+    public void register_should_register_watcher_for_StandardWatchEventKinds() {
+        sut.register(pathDependency);
+        try {
+            verify(pathDependency).register(sut.getWatcher(), ENTRY_CREATE, ENTRY_MODIFY );
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+
+
+    @Test
+    public void register_should_store_created_WatchKey() {
+        WatchKey key = getWatchKey();
+        assertThat(sut.getKeys(), not(hasKey(key)));
+        sut.register(pathDependency);
+        assertThat(sut.getKeys(), hasKey(key));
+
+    }
+
+    @Test
+    public void register_should_map_created_WatchKey_to_Path() {
+        WatchKey key = getWatchKey();
+        sut.register(pathDependency);
+        assertThat(sut.getKeys().get(key), is(pathDependency));
+
+    }
+
+    @Test
+    public void kill_should_prevent_run_loop() throws IOException {
+        Path path = FileSystems.getDefault().getPath("C:/Temp");
+        sut.register(path);
+        sut.kill();
+        sut.run();
+    }
+
+//    @Test
+//    public void
+    private WatchKey getWatchKey() {
+        WatchKey key = mock(WatchKey.class);
+
+        try {
+            stub(pathDependency.register(any(WatchService.class), any(WatchEvent.Kind.class),any(WatchEvent.Kind.class))).toReturn(key);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return key;
+    }
+}
